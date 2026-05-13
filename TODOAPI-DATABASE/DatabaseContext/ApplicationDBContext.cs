1fff603c01573fdb79395c7f5aa1366eed8a7acc
Precious Nwajei <peculiarpreciouslee@gmail.com>
2026-05-13 23:56:20 +0200

    Installed EF Core Packages

---
+++ TODOAPI-DATABASE/DatabaseContext/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TODOAPI_DATABASE.Models;
+namespace TODOAPI_DATABASE.DBContext
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
